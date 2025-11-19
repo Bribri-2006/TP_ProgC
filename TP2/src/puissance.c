8cf9f4071753e3a4d9d02dc563fc1f12dedbc787
Bribri-2006 <britanylauraine4@gmail.com>
2025-11-19 15:09:15 +0100

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2;        // valeur de base
-    int b = 3;        // exposant
-    int resultat = 1; // initialisation du résultat
+    int a = 2;       // base
+    int b = 3;       // exposant
+    int resultat = 1;
 
     // Calcul de a^b
     for (int i = 0; i < b; i++) {
         resultat = resultat * a;
     }
 
-    // Affichage du résultat
     printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
 
     return 0;
